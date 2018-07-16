d840a0d13cb05ac3d6fe8d47f8897c7d68c86678
Ranjit Shinde <ranjitshinde@in-ranjitbs.local>
2018-07-16 18:21:14 +0530

    now showing movie support

--- src/app/rootReducer.js
+++ src/app/rootReducer.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import movies from '../movies/reducer';
-
 const rootReducer = combineReducers({
   movies,
   routing: routerReducer,
