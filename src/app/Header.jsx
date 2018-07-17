82997674c4012dc7545f290a368d3ff979c86707
sumit-kumar14 <sumitgupta.gupta14@gmail.com>
2018-07-17 12:52:38 +0530

    Added location and language selection

--- src/app/Header.jsx
+++ src/app/Header.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
 const Header = () => (
-  <nav className="navbar navbar-expand-lg navbar-light bg-primary navbar-dark"> <a class="navbar-brand" href="/">Just Cinemas</a> </nav>
+  <nav className="navbar navbar-light navbar navbar-dark bg-primary justify-content-between">
+    <a className="navbar-brand" href="/">Navbar</a>
+
+    <form class="form-inline">
+        <select className="form-control mr-sm-2">
+          <option>Select Language</option>
+          <option value="English">English</option>
+          <option value="Hindi">Hindi</option>
+        </select>
+
+        <select className="form-control mr-sm-2">
+          <option>Select Location</option>
+          <option value="Pune">Pune</option>
+          <option value="Delhi">Delhi</option>
+          <option value="Bangalore">Bangalore</option>
+        </select>
+    </form>
+  </nav>
 );
 
 Header.defaultProps = {};
