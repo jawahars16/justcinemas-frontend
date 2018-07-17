ac0e786fffffe1a7cbefd8559aa8bfac0ed5bb01
sumit-kumar14 <sumitgupta.gupta14@gmail.com>
2018-07-17 11:00:25 +0530

    Added click event

--- src/app/Header.jsx
+++ src/app/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Header = () => (
-  <nav className="navbar navbar-expand-lg navbar-light bg-primary navbar-dark"> <a class="navbar-brand" href="#">Just Cinemas</a> </nav>
+  <nav className="navbar navbar-expand-lg navbar-light bg-primary navbar-dark"> <a class="navbar-brand" href="/">Just Cinemas</a> </nav>
 );
 
 Header.defaultProps = {};
