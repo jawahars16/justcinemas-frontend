1dde9a7104adb609a1ef3d2feab143a242881044
Babu Jawahar Suresh <babujawaharsuresh@INBabu-Jawahar-Suresh.local>
2018-07-17 12:49:57 +0530

    Added style for menu bar

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -8,23 +8,22 @@ import Header from "./Header";
 import Home from "./Home";
 import MovieDetail from "../movies/MovieDetail";
 import UpComingMovie from "./UpComing";
-import MenuBar from './MenuBar'
+import MenuBar from "./MenuBar";
 
 const browserHistory = createHistory();
-
 
 const store = configureStore(browserHistory);
 
 const Routes = () => (
   <ConnectedRouter history={browserHistory}>
-  <div>
-  <MenuBar/>
-  <br />
-    <Switch>
-      <Route exact component={Home} path="/" />
-      <Route component={UpComingMovie} path="/upcoming-movie" />
-      <Route component={MovieDetail} path="/movie/:id" />
-    </Switch>
+    <div>
+      <MenuBar />
+      <br />
+      <Switch>
+        <Route exact component={Home} path="/" />
+        <Route component={UpComingMovie} path="/upcoming-movie" />
+        <Route component={MovieDetail} path="/movie/:id" />
+      </Switch>
     </div>
   </ConnectedRouter>
 );
--- src/app/MenuBar.jsx
+++ src/app/MenuBar.jsx
@@ -1,26 +1,45 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import {withRouter} from 'react-router-dom';
+import { withRouter } from "react-router-dom";
 
 class MenuBar extends React.Component {
-	constructor(props) {
-		super(props)
-	}
+  constructor(props) {
+    super(props);
+  }
 
-    nextPath(path) {
-         this.props.history.push(path);
-    }
-	render() {
-		return (
-               <div>
-                    <button type="button" className="btn btn-outline-info" onClick={() => this.nextPath('/')}>Now Showing</button>
-                    <button type="button" className="btn btn-outline-info" onClick={() => this.nextPath('/upcoming-movie')}>Coming Soon </button>
-               </div>
-              )
-	}
+  nextPath(path) {
+    this.props.history.push(path);
+  }
+  render() {
+    return (
+      <div className="row">
+        <div className="col-lg-5" />
+        <div className=" col-lg-2">
+          <div class="btn-group" role="group" aria-label="Basic example">
+            <button
+              type="button"
+              className={`btn btn-primary  ${
+                window.location.pathname == "/" ? "active" : ""
+              }`}
+              onClick={() => this.nextPath("/")}
+            >
+              Now Showing
+            </button>
+            <button
+              type="button"
+              className={`btn btn-primary  ${
+                window.location.pathname == "/upcoming-movie" ? "active" : ""
+              }`}
+              onClick={() => this.nextPath("/upcoming-movie")}
+            >
+              Coming Soon
+            </button>
+          </div>
+        </div>
+        <div className="col-lg-5" />
+      </div>
+    );
+  }
 }
 
-
-export default withRouter(MenuBar)
-
- 
+export default withRouter(MenuBar);
