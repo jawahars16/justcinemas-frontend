13d651fbffadcec5dba28b6e4b4c0c4536114433
Babu Jawahar Suresh <babujawaharsuresh@INBabu-Jawahar-Suresh.local>
2018-07-18 17:40:49 +0530

    Fixed test failures

--- config/jest.setup.js
+++ config/jest.setup.js
@@ -3,12 +3,8 @@ import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
 
-window.localStorage = () => {
-  store = {};
-  setItem = (key, val) => (store[key] = val);
-  getItem = key => store[key];
-  removeItem = key => {
-    delete store[key];
-  };
-  clear = () => (store = {});
+window.localStorage = {
+  getItem: function() {
+    return "";
+  }
 };
